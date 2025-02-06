530b1cd161800aa9dd3e4677cd81001e0228957c
tomi-0 <asadetomi@gmail.com>
2025-02-06 14:05:09 +0000

    added functions

--- src/NumberGuesserGame.java
+++ src/NumberGuesserGame.java
@@ -2,4 +2,12 @@ public class NumberGuesserGame {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int generateNumber(){
+        return 1;
+    }
+
+    public static void userGuess(){
+        
+    }
 }
